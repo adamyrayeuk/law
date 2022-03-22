895fc6d2bfd74d77cdc34f467b0cc48e298d5b24
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2022-03-22 18:28:00 +0700

    convert string to json

---
+++ apps/courses/views.py
@@ -0,0 +1,99 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Course
+
+import json
+
+@api_view(['POST'])
+def create(request):
+    data = json.loads(request.data.get('_content'))
+    course_id = data.get("course_id")
+    nama = data.get("nama")
+    description = data.get("description")
+
+    if not (course_id and nama and description):
+        response = {
+            'status': 400,
+            'message': 'Please provide course_id, nama, and description'
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    course = Course.objects.create(course_id=course_id, nama=nama, description=description)
+    course.save()
+
+    response = {
+        'status': 201,
+        'messsage': 'Course created'
+    }
+    return Response(response, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def read(request, course_id):
+    try:
+        course = Course.objects.get(course_id=course_id)
+    except Course.DoesNotExist:
+        response = {
+            'status': 400,
+            'message': 'The course does not exist'
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    response = {
+        'status': 200,
+        'course_id': course.course_id,
+        'nama': course.nama,
+        'description': course.description
+    }
+    return Response(response, status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+def update(request, course_id):
+    try:
+        course = Courses.objects.get(course_id=course_id)
+    except Course.DoesNotExist:
+        reponse = {
+            'status': 400,
+            'message': 'The course does not exist'
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    if not (request.data.get("nama") or request.data.get("description")):
+        response = {
+            'status': 400,
+            'message': 'Please provide the field you want to update'
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    nama = request.data.get("nama", course.nama)
+    description = request.data.get("description", course.description)
+
+    course.nama = nama
+    course.description = description
+    course.save()
+
+    response = {
+        'status': 200,
+        'message': 'The course has been updated'
+    }
+    return Response(response, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def delete(request, course_id):
+    try:
+        course = Course.objects.get(course_id=course_id)
+    except Course.DoesNotExist:
+        response = {
+            'status': 400,
+            'message': 'The course does not exist'
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    course.delete()
+
+    response = {
+        'status': 200,
+        'message': 'Course deleted'
+    }
+    return Response(response, status=status.HTTP_200_OK)
