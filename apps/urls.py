2af963cabe49d22708250402e7b9dd2a9813c3dc
adamyrayeuk <64898109+adamyrayeuk@users.noreply.github.com>
2022-03-22 15:08:00 +0700

    Staging (#7)
    * Implement OAuth Service and Custom User Model (#1) (#2)
    
    * Create custom user
    
    * Create OAuth service
    
    * Set urls to access API
    
    * Set auth model used, register oauth app, set default parser for rest, and set url to access API
    
    * Development (#6)
    
    * Implement OAuth Service and Custom User Model (#1)
    
    * Create custom user
    
    * Create OAuth service
    
    * Set urls to access API
    
    * Set auth model used, register oauth app, set default parser for rest, and set url to access API
    
    * Courses (#4)
    
    * Create custom user
    
    * Create OAuth service
    
    * Set urls to access API
    
    * Set auth model used, register oauth app, set default parser for rest, and set url to access API
    
    * Implement CRUD courses
    
    * Oauth (#5)
    
    * Create custom user
    
    * Create OAuth service
    
    * Set urls to access API
    
    * Set auth model used, register oauth app, set default parser for rest, and set url to access API
    
    * Implement CRUD courses
    
    * resolve conflict

--- apps/urls.py
+++ apps/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('account/', include('apps.accounts.urls')),
     path('oauth/', include('apps.oauth.urls')),
+    path('course/', include('apps.courses.urls')),
 ]
