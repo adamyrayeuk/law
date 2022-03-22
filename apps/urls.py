3fa31ee45c2394bdb6c0f92ab6e195d5b2b2250a
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2022-03-22 15:01:12 +0700

    resolve conflict

--- apps/urls.py
+++ apps/urls.py
@@ -3,8 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('account/', include('apps.accounts.urls')),
     path('oauth/', include('apps.oauth.urls')),
-<<<<<<< HEAD
     path('course/', include('apps.courses.urls')),
-=======
->>>>>>> a961fa4083eca7b33c333dc742602349a1399467
 ]
